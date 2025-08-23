e3ecb9225ff37884e281d89e5be8410312e156de
JavaThugNGG <ildarjava@mail.ru>
2025-08-23 16:39:22 +0300

    feat: add word managing logic

---
+++ Hangman/WordDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class WordDictionary
+    {
+        private readonly Random random = new Random();
+        private readonly Dictionary<string, string> words = new();
+
+        internal WordDictionary(string pathName)
+        {
+            using var reader = new StreamReader(pathName);
+            ReadWords(reader);
+        }
+
+        internal string GetRandomWord()
+        {
+            int index = random.Next(words.Count);
+            return words.Keys.ElementAt(index);
+        }
+
+        internal string GetDefinition(string word)
+        {
+            return words[word];
+        }
+
+        private void ReadWords(StreamReader reader)
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split(" - ");
+                words.Add(parts[0], parts[1]);
+            }
+        }
+    }
+}
