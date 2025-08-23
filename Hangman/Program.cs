cf7120bb3e58aa1a1d6665011853acbeccfe9121
JavaThugNGG <ildarjava@mail.ru>
2025-08-23 16:38:39 +0300

    feat: add entry point Main method

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Hangman
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private const string Path = "words.txt";
+
+        internal static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = System.IO.File.ReadAllText(Path);
+            Console.WriteLine(text);
         }
     }
 }
